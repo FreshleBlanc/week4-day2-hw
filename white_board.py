e0717730fdac836a61bf1446786fe949611e7aef
FreshleBlanc <dchang4309@gmail.com>
2022-11-30 17:11:40 -0600

    first commit

---
+++ white_board.py
@@ -0,0 +1,84 @@
+""" 
+Reformat the String
+Given alphanumeric string s. (Alphanumeric string is a string consisting of lowercase English letters and digits).
+You have to find a permutation of the string where no letter is followed by another letter and no digit is followed by another digit. That is, no two adjacent characters have the same type.
+Return the reformatted string or return an empty string if it is impossible to reformat the string.
+Example 1:
+Input: s = "a0b1c2"
+Output: "0a1b2c"
+Explanation: No two adjacent characters have the same type in "0a1b2c". "a0b1c2", "0a1b2c", "0c2a1b" are also valid permutations.
+Example 2:
+Input: s = "leetcode"
+Output: ""
+Explanation: "leetcode" has only characters so we cannot separate them by digits.
+Example 3:
+Input: s = "1229857369"
+Output: ""
+Explanation: "1229857369" has only digits so we cannot separate them by characters.
+Example 4:
+Input: s = "covid2019"
+Output: "c2o0v1i9d"
+Example 5:
+Input: s = "ab123"
+Output: "1a2b3
+"""
+
+def reformat(str_1):
+    
+    digits = []
+    letters = []
+    for character in str_1:    
+        if character.isdigit():
+            digits.append(character)
+        else: 
+            letters.append(character)
+
+        
+        
+        # if digits > than letters 
+        # then digits 
+        # else letters first
+    
+    current = "digit"
+    if len(digits) > len(letters):
+        current ="digit"
+    else:
+        current = "letter"
+  
+    if len(digits) + 1 < len(letters):
+        return ''
+
+    if len(letters) + 1 < len(digits):
+        return ''
+    
+    remix = []
+
+    for i in range(len(str_1)):
+        if current == "digit":
+            digit = digits.pop()
+            remix.append(digit)
+            current = "letter"
+        else:
+            letter = letters.pop()
+            remix.append(letter)
+            current = "digit"
+    return remix
+
+
+    # letters = ['a','b','c'], digits = ['1','2'] -> 'a1b2c'
+    # letters = ['a','b'], digits = ['1','2','3'] -> '1a2b3'
+        # character goes into digits
+
+
+    # letters = ['a','b'], digits = ['1','2','3','4'] -> '1a2b34'
+
+
+# print(reformat("a0b1c2"))
+print(reformat("ab1234"))
+# print(reformat("1229857369"))
+# print(reformat("covid2019"))
+# print(reformat("ab123"))
+
+
+#.isdigit
+#.isalpha
